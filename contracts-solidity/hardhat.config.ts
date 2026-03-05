3a75303f93362b1a69023af62dca009bad8157e0
hariramsss <hariram.27csa@licet.ac.in>
2026-03-06 00:15:34 +0530

    setting up the environment part-1(to be continued)

---
+++ contracts-solidity/hardhat.config.ts
@@ -0,0 +1,38 @@
+import { HardhatUserConfig } from "hardhat/config"
+import "@nomicfoundation/hardhat-toolbox"
+import "@parity/hardhat-polkadot"
+
+const config: HardhatUserConfig = {
+    solidity: "0.8.28",
+    networks: {
+        hardhat: {
+            polkadot: {
+                target: "evm",
+            },
+            nodeConfig: {
+                nodeBinaryPath: "./bin/dev-node",
+                rpcPort: 8000,
+                dev: true,
+            },
+            adapterConfig: {
+                adapterBinaryPath: "./bin/eth-rpc",
+                dev: true,
+            },
+        },
+        localNode: {
+            polkadot: {
+                target: "evm",
+            },
+            url: `http://127.0.0.1:8545`,
+        },
+        polkadotHubTestnet: {
+            polkadot: {
+                target: "evm",
+            },
+            url: "https://testnet-passet-hub-eth-rpc.polkadot.io",
+            accounts: [vars.get("PRIVATE_KEY")],
+        },
+    },
+}
+
+export default config
